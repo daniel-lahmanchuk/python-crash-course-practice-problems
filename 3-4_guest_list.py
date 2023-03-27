c23700ed14944c470bb95a5636ad72035f857a3a
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-03-27 10:33:48 -0700

    Add files via upload

---
+++ 3-4_guest_list.py
@@ -0,0 +1,9 @@
+# 3-4. Guest List:  "If you could invite anyone, living or deceased, to dinner, who would you invite? 
+Make a list that includes at least three people you’d like to invite to dinner Then use your list to print 
+a message to each person, inviting them to dinner."
+
+guest_list = ['Albert Einstein', 'Isaac Newton', 'Stephen Hawking']
+
+print(f"{guest_list[0]} is invited to dinner.")
+print(f"{guest_list[1]} is invited to dinner.")
+print(f"{guest_list[2]} is invited to dinner.")
