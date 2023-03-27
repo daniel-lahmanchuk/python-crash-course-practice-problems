4c3204fb3730f609beab579bf49fa10bf8c82b58
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-03-27 15:19:34 -0700

    Add files via upload

---
+++ 3-5_changing_guest_list.py
@@ -0,0 +1,14 @@
+# 3-5. Changing Guest List:  "You just heard that one of your guests can’t make the dinner, so you need 
+to send out a new set of invitations. You’ll have to think of someone else to invite"
+
+guest_list = ['Albert Einstein', 'Isaac Newton', 'Stephen Hawking']
+
+print(f"{guest_list[0]} is invited to dinner.")
+print(f"{guest_list[1]} is invited to dinner.")
+print(f"{guest_list[2]} can't make it to dinner.")
+
+guest_list.append('Neil deGrasse Tyson')
+
+print(f"\n{guest_list[0]} is invited to dinner.")
+print(f"{guest_list[1]} is invited to dinner.")
+print(f"{guest_list[3]} is invited to dinner.")
