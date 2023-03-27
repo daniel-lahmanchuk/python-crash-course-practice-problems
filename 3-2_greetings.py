a4045db4d8a487edef24b774c1d2e582a58f06db
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-03-27 09:56:43 -0700

    Add files via upload

---
+++ 3-2_greetings.py
@@ -0,0 +1,11 @@
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person's name, 
+print a message to them. The text of each messsage should be the same, but each message should be personalized 
+with the person's name.
+
+names = ['Ben', 'Peter', 'Jaxon', 'Sebastian', 'Jason']
+
+print(f"I have a friend named {names[0]}.")
+print(f"I have a friend named {names[1]}.")
+print(f"I have a friend named {names[2]}.")
+print(f"I have a friend named {names[3]}.")
+print(f"I have a friend named {names[4]}.")
