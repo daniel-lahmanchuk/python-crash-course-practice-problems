18de684fe637415a949c8a84b43c51aa14ae0685
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-03-27 08:03:45 -0700

    Add files via upload

---
+++ 2-10_adding_comments.py
@@ -0,0 +1,8 @@
+# 2-10. Adding Comments: Choose two of the programs you've written, and add at least one comment to each. If you don't have anything to specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+# Created By: Daniel Lahmanchuk
+# Created On: 3/27/2023
+
+# What It Does: It's printing a statement that I decided, in this case it's "Hello World!".
+
+print("Hello World!")
