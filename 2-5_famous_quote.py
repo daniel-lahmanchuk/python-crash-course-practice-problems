d4a86810ecd82f7fde5d6fb569df8aa1e3bec817
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-03-26 19:58:33 -0700

    Add files via upload

---
+++ 2-5_famous_quote.py
@@ -0,0 +1,5 @@
+# 2-5 Famous Quote: Find a quote froma famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks.
+
+quote = "'I can accept failure, everyone fails at something. But I can't accept not trying.' - Michael Jordan"
+
+print(quote)
