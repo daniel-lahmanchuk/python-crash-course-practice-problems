9fe64855725936cfff14c72c51cfd8e9b94ea172
Mr-Savant <124419231+Mr-Savant@users.noreply.github.com>
2023-03-26 19:35:31 -0700

    Add files via upload

---
+++ 2-2_simple_messages.py
@@ -0,0 +1,7 @@
+# 2-2. Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+message = "Hello World!"
+print(message)
+
+message = "Hello User!"
+print(message)
