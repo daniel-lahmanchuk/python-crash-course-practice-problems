328e37e13432a6ecdf90c5f75539b6d4613da861
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-04-28 21:27:37 -0700

    Add files via upload

---
+++ 3-11_intentional_error.py
@@ -0,0 +1,5 @@
+# 3-11. Intentional Error: If you haven't received an index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+
+names = ['Daniel', 'Ben', 'Peter']
+
+print(names[3])
