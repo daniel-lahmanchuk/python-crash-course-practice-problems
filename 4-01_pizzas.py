e5a9c737fdca9cc29479cd4c7ca72b0c483083d2
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-04-28 21:33:03 -0700

    Add files via upload

---
+++ 4-01_pizzas.py
@@ -0,0 +1,8 @@
+# 4-1. Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a list, and then use a for loop to print the name of each pizza.
+
+pizza_types = ['Cheese', 'Pepperoni', 'Mushrooms and Olives']
+
+for pizza in pizza_types:
+    print(f"One of my favorite types of pizza is: {pizza}")
+
+print("I really like pizza!")
