592bd4dc057d2ffddc04c49275186e4d3287033b
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-04-28 21:01:24 -0700

    Add files via upload

---
+++ 3-08_seeing_the_world.py
@@ -0,0 +1,17 @@
+# 3-8. Think of at least five places in the world you'd like to visit.
+
+places = ['Paris, France', 'Tokyo, Japan', 'Barcelona, Spain', 'Berlin, Germany', 'Rome, Italy']
+
+print(places)
+print(sorted(places))
+print(places)
+print(sorted(places, reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
