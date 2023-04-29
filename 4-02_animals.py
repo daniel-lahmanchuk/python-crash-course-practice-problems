e9e33f73837f64e91f7466a71d75d1e5a4bb661d
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-04-28 21:36:54 -0700

    Add files via upload

---
+++ 4-02_animals.py
@@ -0,0 +1,8 @@
+# 4-2. Animals: Think of at least three different animals that have a certain characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+
+animals = ['dog', 'cat', 'parrot']
+
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("Any of these animals would make a great pet!")
