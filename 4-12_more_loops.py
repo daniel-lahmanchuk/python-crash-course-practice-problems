c51c7fb35f3ad537b809ba51d838c6a345deabfb
Daniel-Lahmanchuk <124419231+Daniel-Lahmanchuk@users.noreply.github.com>
2023-05-01 08:44:49 -0700

    Add files via upload

---
+++ 4-12_more_loops.py
@@ -0,0 +1,15 @@
+My favorite foods are:
+pizza
+falafel
+carrot cake
+cannoli
+
+My friend's favorite foods are:
+pizza
+falafel
+carrot cake
+ice cream
+
+
+** Process exited - Return Code: 0 **
+Press Enter to exit terminal
