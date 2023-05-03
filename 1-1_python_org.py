c77307572c81a43a5fd1a6f84b0fd41757bfc930
daniel-lahmanchuk <124419231+daniel-lahmanchuk@users.noreply.github.com>
2023-05-03 12:59:49 -0700

    Create 1-1_python_org.py

---
+++ 1-1_python_org.py
@@ -0,0 +1,1 @@
+# 1-1. python.org: Explore the Python home page (https://python.org) to find topics that interest you. As you become familiar with Python, different parts of the site will become more useful to you.
