84c0c07ef4074a7d631e9150cf35726e0b9f6cf1
Daniel Lahmanchuk <danlahmanchuk@tutanota.com>
2023-05-26 17:09:02 +0000

    4-13_buffet.py

---
+++ 4-13_buffet.py
@@ -0,0 +1,11 @@
+# 4-13. Buffet: A buffet-style restaurant offers only five basic foods. Think of five simple foods, and store them in a tuple.
+
+foods = ("eggs", "french toast", "waffles", "cereal", "pancakes")
+
+for food in foods:
+    print(food)
+
+foods = ("eggs", "bagel", "orange juice", "sandwich", "pancakes")
+
+for food in foods:
+    print(food)
